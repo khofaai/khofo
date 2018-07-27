65d31352b78ccdeee2056961f715c165d5969046
anaselalami <anaselalamikh@gmail.com>
2018-07-27 16:26:30 +0100

    add comment for each function / variable

--- src/core/Commands/LarasetCommands.php
+++ src/core/Commands/LarasetCommands.php
@@ -3,226 +3,218 @@
 namespace Khofaai\Laraset\core\Commands;
 
 use Illuminate\Console\Command;
-
 use Laraset;
 use File;
 
-abstract class LarasetCommands extends Command
-{
-	/**
-	 * This Core name
-	 * 
-	 * @var string
-	 */
-	protected $coreNamespace = "laraset";
-	/**
-	 * project base path 
-	 * 
-	 * @var String
-	 */
-	protected $basePath;
-	/**
-	 * [$modulePath description]
-	 * @var Strnig
-	 */
-	protected $modulePath;
-	/**
-	 * contain all command options
-	 * @var array
-	 */
-	protected $commandOptions = [];
-	/**
-	 * Module name
-	 * @var String
-	 */
-	protected $moduleName;
-	/**
-	 * Module name with first Letter uppercase
-	 * @var String
-	 */
-	protected $moduleNameUpper;
-	/**
-	 * modules folder base path
-	 * @var String
-	 */
-	protected $baseSrc;
+abstract class LarasetCommands extends Command {
 
-	public function __construct() 
-	{
-		$this->basePath = app_path($this->coreNamespace).'/';
-		parent::__construct();
-	}
-	/**
-	 * Execute the console command.
-	 */
-	abstract function handle();
+    /**
+     * This Core name
+     * 
+     * @var string
+     */
+    protected $coreNamespace = "laraset";
 
-	/**
-	 * Init module name in case is allowed
-	 * 
-	 * @return Boolean
-	 */
-	protected function init() 
-	{	
+    /**
+     * project base path 
+     * 
+     * @var String
+     */
+    protected $basePath;
+
+    /**
+     * [$modulePath description]
+     * @var Strnig
+     */
+    protected $modulePath;
+
+    /**
+     * contain all command options
+     * @var array
+     */
+    protected $commandOptions = [];
+
+    /**
+     * Module name
+     * @var String
+     */
+    protected $moduleName;
+
+    /**
+     * Module name with first Letter uppercase
+     * @var String
+     */
+    protected $moduleNameUpper;
+
+    /**
+     * modules folder base path
+     * @var String
+     */
+    protected $baseSrc;
+
+    public function __construct() {
+        $this->basePath = app_path($this->coreNamespace) . '/';
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    abstract function handle();
+
+    /**
+     * Init module name in case is allowed
+     * 
+     * @return Boolean
+     */
+    protected function init() {
         $this->initName();
         if (in_array(strtolower($this->moduleName), $this->notAllowedNames())) {
-            $this->info('<options=bold;fg=yellow>['.strtolower($this->moduleName).']<bg=black;fg=yellow> name is reserved ! please choose another one');
+            $this->info('<options=bold;fg=yellow>[' . strtolower($this->moduleName) . ']<bg=black;fg=yellow> name is reserved ! please choose another one');
             return false;
         }
         return true;
-	}
+    }
 
-	/**
-	 * Create File
-	 * 
-	 * @param  String $path
-	 * @param  String $content
-	 * @return void
-	 */
-	protected function makeFile($path,$content) 
-	{
-		File::put($path,$content);
-	}
+    /**
+     * Create File
+     * 
+     * @param  String $path
+     * @param  String $content
+     * @return void
+     */
+    protected function makeFile($path, $content) {
+        File::put($path, $content);
+    }
 
-	/**
-	 * return not allowed names
-	 * 
-	 * @return Array
-	 */
-	protected function notAllowedNames()
-	{
-		return [
-			'admin'
-		];
-	}
+    /**
+     * return not allowed names
+     * 
+     * @return Array
+     */
+    protected function notAllowedNames() {
+        return [
+            'admin'
+        ];
+    }
 
-	/**
-	 * Initial module name
-	 * 
-	 * @return void
-	 */
-	protected function initName() 
-	{
-		$this->moduleName = $this->formatName($this->argument('name'));
-		$this->moduleNameUpper = ucfirst(camel_case($this->moduleName));
-		
-		$this->baseSrc =  $this->basePath.'modules/';
-		$this->modulePath = $this->baseSrc.$this->moduleName;
-	}
+    /**
+     * Initial module name
+     * 
+     * @return void
+     */
+    protected function initName() {
+        $this->moduleName = $this->formatName($this->argument('name'));
+        $this->moduleNameUpper = ucfirst(camel_case($this->moduleName));
 
-	/**
-	 * set option name to $commandOptions
-	 * 
-	 * @param String $option
-	 * @return void
-	 */
-	protected function setOption($option) 
-	{
-		$optionValue = $this->option($option);
-		$this->commandOptions[$option] = ($optionValue == 'default' ? false : (is_null($optionValue) ? true : $optionValue));
-	}
+        $this->baseSrc = $this->basePath . 'modules/';
+        $this->modulePath = $this->baseSrc . $this->moduleName;
+    }
 
-	/**
-	 * get option value
-	 * 
-	 * @param  String $option option name
-	 * @return Boolean/String ( String in case the option has value )
-	 */
-	protected function getOption($option)
-	{
-		return $this->commandOptions[$option];
-	}
+    /**
+     * set option name to $commandOptions
+     * 
+     * @param String $option
+     * @return void
+     */
+    protected function setOption($option) {
+        $optionValue = $this->option($option);
+        $this->commandOptions[$option] = ($optionValue == 'default' ? false : (is_null($optionValue) ? true : $optionValue));
+    }
 
-	/**
-	 * Set all command options to $commandOptions
-	 * 
-	 * @param Array/String $option ( Array in case multiple options )
-	 */
-	protected function setCommandOption($option) 
-	{
-		if (is_array($option)) {
-			foreach ($option as $opt) {
-				$this->setOption($opt);
-			}
-		} else {
-			$this->setOption($opt);
-		}
-	}
+    /**
+     * get option value
+     * 
+     * @param  String $option option name
+     * @return Boolean/String ( String in case the option has value )
+     */
+    protected function getOption($option) {
+        return $this->commandOptions[$option];
+    }
 
-	/**
-	 * Remplace - with _
-	 * 
-	 * @param  String $name
-	 * @return String
-	 */
-	protected function formatName($name) 
-	{
-		return str_replace('-', '_', $name); 
-	}
+    /**
+     * Set all command options to $commandOptions
+     * 
+     * @param Array/String $option ( Array in case multiple options )
+     */
+    protected function setCommandOption($option) {
+        if (is_array($option)) {
+            foreach ($option as $opt) {
+                $this->setOption($opt);
+            }
+        } else {
+            $this->setOption($opt);
+        }
+    }
 
-	/**
-	 * Create Directory
-	 * 
-	 * @param  String $path
-	 * @return void
-	 */
-	protected function makeDir($path) 
-	{
-		File::makeDirectory( $path, 0777, true, true );
-	}
+    /**
+     * Remplace - with _
+     * 
+     * @param  String $name
+     * @return String
+     */
+    protected function formatName($name) {
+        return str_replace('-', '_', $name);
+    }
 
-	/**
-	 * Create directories
-	 *
-	 * @param  array $directories
-	 * @return void
-	 */
-	protected function makeDirectories($directories) 
-	{
-		$this->makeDir($this->modulePath);
-		foreach ($directories as $directory) {
-			$this->makeDir($this->modulePath . '/' . $directory);
-		}
-	}
+    /**
+     * Create Directory
+     * 
+     * @param  String $path
+     * @return void
+     */
+    protected function makeDir($path) {
+        File::makeDirectory($path, 0777, true, true);
+    }
 
-	/**
-	 * get Laraset Modules name
-	 * 
-	 * @return qrray
-	 */
-	protected function modulesName() 
-	{
-		return array_keys(Laraset::modules());
-	}
+    /**
+     * Create directories
+     *
+     * @param  array $directories
+     * @return void
+     */
+    protected function makeDirectories($directories) {
+        $this->makeDir($this->modulePath);
+        foreach ($directories as $directory) {
+            $this->makeDir($this->modulePath . '/' . $directory);
+        }
+    }
 
-	/**
-	 * get Instance Signature
-	 * 
-	 * @return string
-	 */
-	public function getSignature() 
-	{
-		return $this->signature;
-	}
+    /**
+     * get Laraset Modules name
+     * 
+     * @return qrray
+     */
+    protected function modulesName() {
+        return array_keys(Laraset::modules());
+    }
 
-	/**
-	 * Get instance description
-	 *
-	 * @return string
-	 */
-	public function getDescription() 
-	{
-		return $this->description;
-	}
+    /**
+     * get Instance Signature
+     * 
+     * @return string
+     */
+    public function getSignature() {
+        return $this->signature;
+    }
 
-	/**
-	 * Get given stub file name
-	 *
-	 * @param  string $name
-	 * @return string
-	 */
-	protected function getStubFileContent($name) 
-	{
-		return File::get(Laraset::getStub($name));
-	}
+    /**
+     * Get instance description
+     *
+     * @return string
+     */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    /**
+     * Get given stub file name
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function getStubFileContent($name) {
+        return File::get(Laraset::getStub($name));
+    }
+
 }
--- src/core/Commands/LarasetInstall.php
+++ src/core/Commands/LarasetInstall.php
@@ -3,143 +3,165 @@
 namespace Khofaai\Laraset\core\Commands;
 
 use Khofaai\Laraset\core\Commands\LarasetCommands;
-use Illuminate\Filesystem\Filesystem;
-use File,Laraset;
 
-class LarasetInstall extends LarasetCommands
-{
+use File,
+    Laraset;
+
+class LarasetInstall extends LarasetCommands {
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'laraset:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'install laraset logic';
 
-    protected $messages = [
-    	'success' => '<options=bold;fg=cyan>[Laraset]<bg=black;fg=cyan> installed successfully',
-    	'warning' => '<options=bold,reverse>~Laraset~<fg=yellow> already installed.'
-    ];
     /**
-	 * @inheritdoc
-	 */
-	public function handle() 
-	{
-		$this->updateInstallStatus();
-	}
-	/**
-	 * @return [type]
-	 */
-	protected function updateInstallStatus() 
-	{
-		$path = Laraset::path('core.json');
+     * hold success and warning console messages
+     * 
+     * @var array
+     */
+    protected $messages = [
+        'success' => '<options=bold;fg=cyan>[Laraset]<bg=black;fg=cyan> installed successfully',
+        'warning' => '<options=bold,reverse>~Laraset~<fg=yellow> already installed.'
+    ];
+    
+    /**
+     * Hold Laraset file structure
+     * 
+     * @var array
+     */
+    protected $files = [
+        '/' => [
+            'bootstrap.js' => 'js/bootstrap.js',
+            'core.js' => 'js/core.js',
+            'core.json' => 'js/core.json',
+            'routes.js' => 'js/route.js',
+            'routes.php' => 'route.php',
+            'webpack.mix.js' => 'js/webpack.mix.js'
+        ],
+        'helpers' => [
+            'helpers.js' => 'js/helpers.js',
+            'helpers.php' => 'helpers.php'
+        ],
+        'modules' => [
+            'Core.vue' => 'js/vuejs/menu.vue',
+            'menu.vue' => 'js/vuejs/core.vue',
+            'topbar.vue' => 'js/vuejs/topbar.vue'
+        ]
+    ];
 
-		if (File::exists($path)) {
+    /**
+     * @inheritdoc
+     */
+    public function handle() {
+        $this->updateInstallStatus();
+    }
 
-			$modules = json_decode(File::get($path),true);
+    /**
+     * Update core.json instalation status
+     *
+     * @return void
+     */
+    protected function updateInstallStatus() {
+        $path = Laraset::path('core.json');
 
-			if (!$modules['installed']["status"] || !isset($modules['installed']["status"])) {
+        if (File::exists($path)) {
 
-				$this->generateArchitecture();
-				$this->generateFiles();
-				$this->updateWebpackMixJs();
+            $modules = json_decode(File::get($path), true);
 
-				$modules['installed']["status"] = true;
-				$modules['installed']["installed_at"] = date('Y-m-d H:i:s');
-				File::put($path, json_encode((Object)$modules));
+            if (!$modules['installed']["status"] || !isset($modules['installed']["status"])) {
 
-				$this->info($this->messages['success']);
-			} else {
+                $this->generateArchitecture();
+                $this->generateFiles();
+                $this->updateWebpackMixJs();
 
-				$this->info($this->messages['warning']);
-				$this->info('//installed at : '.$modules['installed']['installed_at'].'');
-			}
+                $modules['installed']["status"] = true;
+                $modules['installed']["installed_at"] = date('Y-m-d H:i:s');
+                File::put($path, json_encode((Object) $modules));
 
-		} else {
-			
-			$this->error('core.json not found');
-		}
-	}
+                $this->info($this->messages['success']);
+            } else {
 
-	protected function generateArchitecture() 
-	{
-		$architecture = array(
-			'assets' => '',
-			'dist' => ['js' => ''],
-			'helpers' => '' ,
-			'modules' => ''
-		);
+                $this->info($this->messages['warning']);
+                $this->info('//installed at : ' . $modules['installed']['installed_at'] . '');
+            }
+        } else {
 
-		$path = app_path('laraset');
-		$this->createFolder($path);
-		
-		$this->createArchitectureFolders($architecture,$path);
+            $this->error('core.json not found');
+        }
+    }
 
-		$this->info('* [Folders] Generated Successfully !');
-	}
+    protected function generateArchitecture() {
+        $architecture = array(
+            'assets' => '',
+            'dist' => ['js' => ''],
+            'helpers' => '',
+            'modules' => ''
+        );
 
-	protected function createArchitectureFolders($architecture,$path) 
-	{
-		foreach ($architecture as $key => $value) {
-			$dir_path = $path.'/'.$key;
-			$this->createFolder($dir_path);
-			
-			if (gettype($value) !== 'string') {
+        $path = app_path($this->coreNamespace);
+        $this->createFolder($path);
 
-				$this->createArchitectureFolders($value,$dir_path);
-			}
-		}
-	}
+        $this->createArchitectureFolders($architecture, $path);
 
-	protected function createFolder($path) 
-	{	
-		if (!is_dir($path)) {
-			File::makeDirectory($path);
-		}
-	}
+        $this->info('* [Folders] Generated Successfully !');
+    }
 
-	protected function generateFiles() 
-	{
-		$files = [
-			'/' => 	[
-				'bootstrap.js' => 'js/bootstrap.js',
-				'core.js' => 'js/core.js',
-				'core.json' => 'js/core.json',
-				'routes.js' => 'js/route.js',
-				'routes.php' => 'route.php',
-				'webpack.mix.js' => 'js/webpack.mix.js'
-			],
-			'helpers' => [
-				'helpers.js' => 'js/helpers.js',
-				'helpers.php' => 'helpers.php'
-			],
-			'modules' => [
-				'Core.vue' => 'js/vuejs/menu.vue',
-				'menu.vue' => 'js/vuejs/core.vue',
-				'topbar.vue' => 'js/vuejs/topbar.vue'
-			]
-		];
-		$path = app_path('laraset');
-		foreach ($files as $key => $value) {
-			foreach ($value as $filename => $stub) {
-				$dir_path = $key != '/' ? $path.'/'.$key.'/' : $path.$key;
-				$this->makeFile( $dir_path.$filename, $this->getStubFileContent($stub) );
-			}
-		}
-		
-		$laraset_path = resource_path('views/laraset.blade.php');
-		if (!File::exists($laraset_path)) {
-			$this->makeFile($laraset_path,$this->getStubFileContent('template.blade'));
-		}
+    protected function createArchitectureFolders($architecture, $path) {
+        foreach ($architecture as $key => $value) {
+            $dir_path = $path . '/' . $key;
+            $this->createFolder($dir_path);
 
-		$this->info('* [File] Generated Successfully !');
-	}
+            if (gettype($value) !== 'string') {
 
-	protected function updateWebpackMixJs() {
-		
-		$path = base_path('webpack.mix.js');
+                $this->createArchitectureFolders($value, $dir_path);
+            }
+        }
+    }
 
-		if (File::exists($path)) {
-			$content = File::get($path);
-			$this->makeFile(base_path('webpack-old.mix.js'),$content);
-			File::delete($path);
-		}
+    protected function createFolder($path) {
+        if (!is_dir($path)) {
+            File::makeDirectory($path);
+        }
+    }
 
-		$this->makeFile($path,$this->getStubFileContent('js/webpack.base.mix.js'));
-	}
+    protected function generateFiles() {
+        
+        $path = app_path($this->coreNamespace);
+        foreach ($this->files as $key => $value) {
+            foreach ($value as $filename => $stub) {
+                $dir_path = $key != '/' ? $path . '/' . $key . '/' : $path . $key;
+                $this->makeFile($dir_path . $filename, $this->getStubFileContent($stub));
+            }
+        }
+
+        $laraset_path = resource_path('views/'.$this->coreNamespace.'.blade.php');
+        if (!File::exists($laraset_path)) {
+            $this->makeFile($laraset_path, $this->getStubFileContent('template.blade'));
+        }
+
+        $this->info('* [File] Generated Successfully !');
+    }
+
+    protected function updateWebpackMixJs() {
+
+        $path = base_path('webpack.mix.js');
+
+        if (File::exists($path)) {
+            $content = File::get($path);
+            $this->makeFile(base_path('webpack-old.mix.js'), $content);
+            File::delete($path);
+        }
+
+        $this->makeFile($path, $this->getStubFileContent('js/webpack.base.mix.js'));
+    }
+
 }
